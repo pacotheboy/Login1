99e3dd5b8e11599d2e92b52da648dddcbbc2eeeb
pacotheboy <32809341+pacotheboy@users.noreply.github.com>
2019-06-16 14:46:58 -0500

    Agrege el slider al proyecto

--- app/src/main/java/com/example/login/Usuario.java
+++ app/src/main/java/com/example/login/Usuario.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+
+/*Esta vista es para el usuario una vez registrado*/
 public class Usuario extends AppCompatActivity {
 
     private DrawerLayout nDrawerLayout;
