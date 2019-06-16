a2b8866f458675d03470a33da27fefa4daab8d5f
pacotheboy <32809341+pacotheboy@users.noreply.github.com>
2019-06-16 14:43:46 -0500

    Agrege el slider al proyecto

--- app/src/main/java/com/example/login/Usuario.java
+++ app/src/main/java/com/example/login/Usuario.java
@@ -1,13 +1,33 @@
 package com.example.login;
 
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Usuario extends AppCompatActivity {
 
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
+        nDrawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        nToogle=new ActionBarDrawerToggle(this,nDrawerLayout,R.string.open,R.string.close);
+        nDrawerLayout.addDrawerListener(nToogle);
+        nToogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (nToogle.onOptionsItemSelected(item))
+        {
+            return  true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
